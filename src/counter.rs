fbfae31e309ba9893e01f532f8aa6043e1040ded
Andrea Cracco <guilucand@gmail.com>
2022-03-14 12:07:19 +0200

    Add reset and average counter

--- src/counter.rs
+++ src/counter.rs
@@ -1,37 +1,110 @@
+use lazy_static::lazy_static;
+use parking_lot::lock_api::RawMutex;
+use parking_lot::{Mutex, RwLock};
 use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::marker::PhantomData;
-use parking_lot::{Mutex, RwLock};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Weak};
 use std::thread_local;
-use parking_lot::lock_api::RawMutex;
-use lazy_static::lazy_static;
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! declare_counter_u64_impl {
+    ($name:expr, $mode:ty, $reset:expr, $extra:expr) => {
+        AtomicCounter::<$mode> {
+            __get_counter: || {
+                thread_local! {
+                    static COUNTER: Arc<AtomicU64> = {
+                        let arc = Arc::new(AtomicU64::new(0));
+                        let mut list = $crate::counter::__COUNTERS_LIST.lock();
+                        let mut cvec = list.entry($name.to_string()).or_insert((Vec::new(), <AtomicCounter<$mode> as $crate::counter::__CounterType>::MODE, $reset));
+                        cvec.0.push(Arc::downgrade(&arc));
+                        arc
+                    }
+                }
+                use std::ops::Deref;
+                COUNTER.with(|c| {
+                    unsafe {
+                        &*(c.deref() as *const AtomicU64)
+                    }
+                })
+            },
+            __extra: $extra,
+            _phantom: std::marker::PhantomData
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! declare_counter_u64 {
+    ($name:literal, $mode:ty, $reset:expr) => {
+        $crate::declare_counter_u64_impl!($name, $mode, $reset, ())
+    };
+}
+
+pub(crate) static COUNTER_SUFFIX: &str = "$COUNTER_83uRij";
+
+#[macro_export]
+macro_rules! declare_avg_counter_u64 {
+    ($name:literal, $reset:expr) => {
+        $crate::declare_counter_u64_impl!(
+            $name,
+            $crate::counter::AvgMode,
+            $reset,
+            $crate::declare_counter_u64_impl!(
+                concat!($name, "$COUNTER_83uRij"),
+                $crate::counter::SumMode,
+                $reset,
+                ()
+            )
+        )
+    };
+}
 
 lazy_static! {
     #[doc(hidden)]
-    pub static ref __COUNTERS_LIST: Mutex<HashMap<String, (Vec<Weak<AtomicU64>>, __AcMode)>> = Mutex::new(HashMap::new());
+    pub static ref __COUNTERS_LIST: Mutex<HashMap<String, (Vec<Weak<AtomicU64>>, __AcMode, bool)>> = Mutex::new(HashMap::new());
 }
 
+#[doc(hidden)]
+pub trait AtomicCounterMode {
+    type Extra;
+}
 
-pub enum SumMode {}
+pub struct SumMode {}
+impl AtomicCounterMode for SumMode {
+    type Extra = ();
+}
+pub struct MaxMode {}
+impl AtomicCounterMode for MaxMode {
+    type Extra = ();
+}
+pub struct MinMode {}
+impl AtomicCounterMode for MinMode {
+    type Extra = ();
+}
+pub struct AvgMode {}
+impl AtomicCounterMode for AvgMode {
+    type Extra = AtomicCounter<SumMode>;
+}
 
-pub enum MaxMode {}
-
-pub enum MinMode {}
-
-pub struct AtomicCounter<MODE> {
+pub struct AtomicCounter<MODE: AtomicCounterMode> {
     #[doc(hidden)]
     pub __get_counter: fn() -> &'static AtomicU64,
+    #[doc(hidden)]
+    pub __extra: MODE::Extra,
     #[doc(hidden)]
     pub _phantom: PhantomData<MODE>,
 }
 
 #[doc(hidden)]
+#[derive(Eq, PartialEq)]
 pub enum __AcMode {
     SUM,
     MAX,
     MIN,
+    AVG,
 }
 
 #[doc(hidden)]
@@ -55,7 +128,17 @@ impl AtomicCounter<SumMode> {
     }
 }
 
-impl __CounterType for AtomicCounter<SumMode> { const MODE: __AcMode = __AcMode::SUM; }
+impl AtomicCounter<AvgMode> {
+    #[inline(always)]
+    pub fn add_value(&self, value: u64) {
+        (self.__get_counter)().fetch_add(value, Ordering::Relaxed);
+        self.__extra.inc();
+    }
+}
+
+impl __CounterType for AtomicCounter<SumMode> {
+    const MODE: __AcMode = __AcMode::SUM;
+}
 
 impl AtomicCounter<MaxMode> {
     #[inline(always)]
@@ -64,7 +147,9 @@ impl AtomicCounter<MaxMode> {
     }
 }
 
-impl __CounterType for AtomicCounter<MaxMode> { const MODE: __AcMode = __AcMode::MAX; }
+impl __CounterType for AtomicCounter<MaxMode> {
+    const MODE: __AcMode = __AcMode::MAX;
+}
 
 impl AtomicCounter<MinMode> {
     #[inline(always)]
@@ -73,7 +158,13 @@ impl AtomicCounter<MinMode> {
     }
 }
 
-impl __CounterType for AtomicCounter<MinMode> { const MODE: __AcMode = __AcMode::MIN; }
+impl __CounterType for AtomicCounter<MinMode> {
+    const MODE: __AcMode = __AcMode::MIN;
+}
+
+impl __CounterType for AtomicCounter<AvgMode> {
+    const MODE: __AcMode = __AcMode::AVG;
+}
 
 pub struct AtomicCounterGuardSum<'a> {
     value: u64,
@@ -83,10 +174,7 @@ pub struct AtomicCounterGuardSum<'a> {
 impl<'a> AtomicCounterGuardSum<'a> {
     pub fn new(counter: &'a AtomicCounter<SumMode>, value: u64) -> Self {
         counter.inc_by(value);
-        Self {
-            value,
-            counter,
-        }
+        Self { value, counter }
     }
 }
 
@@ -96,59 +184,45 @@ impl<'a> Drop for AtomicCounterGuardSum<'a> {
     }
 }
 
-#[macro_export]
-macro_rules! declare_counter_u64 {
-    ($name:expr, $mode:ty) => {
-        AtomicCounter::<$mode> {
-            __get_counter: || {
-                thread_local! {
-                    static COUNTER: Arc<AtomicU64> = {
-                        let arc = Arc::new(AtomicU64::new(0));
-                        let mut list = $crate::counter::__COUNTERS_LIST.lock();
-                        let mut cvec = list.entry($name.to_string()).or_insert((Vec::new(), <AtomicCounter<$mode> as $crate::counter::__CounterType>::MODE));
-                        cvec.0.push(Arc::downgrade(&arc));
-                        arc
-                    }
-                }
-                use std::ops::Deref;
-                COUNTER.with(|c| {
-                    unsafe {
-                        &*(c.deref() as *const AtomicU64)
-                    }
-                })
-            },
-            _phantom: std::marker::PhantomData
-        }
-    }
-}
-
-pub fn get_counter_value(name: &str) -> u64 {
+pub fn get_counter_value(name: &str) -> (u64, u64) {
     let mut counters = __COUNTERS_LIST.lock();
 
-    let (ref mut vec, mode) = if let Some(val) = counters.get_mut(name) {
+    let (ref mut vec, mode, reset) = if let Some(val) = counters.get_mut(name) {
         val
     } else {
-        return 0;
+        return (0, 0);
     };
 
-    let mut result = match mode {
-        __AcMode::SUM => { 0 },
-        __AcMode::MAX => { 0 },
-        __AcMode::MIN => { u64::MAX },
+    let reset_value = match mode {
+        __AcMode::SUM => 0,
+        __AcMode::MAX => 0,
+        __AcMode::MIN => u64::MAX,
+        __AcMode::AVG => 0,
     };
+
+    let mut result = reset_value;
 
     vec.retain(|val| {
         if val.strong_count() > 0 {
             if let Some(value) = val.upgrade() {
+                let value = if *reset {
+                    value.swap(reset_value, Ordering::Relaxed)
+                } else {
+                    value.load(Ordering::Relaxed)
+                };
+
                 match mode {
                     __AcMode::SUM => {
-                        result += value.load(Ordering::Relaxed);
+                        result += value;
                     }
                     __AcMode::MAX => {
-                        result = max(result, value.load(Ordering::Relaxed));
+                        result = max(result, value);
                     }
                     __AcMode::MIN => {
-                        result = min(result, value.load(Ordering::Relaxed));
+                        result = min(result, value);
+                    }
+                    __AcMode::AVG => {
+                        result += value;
                     }
                 }
                 true
@@ -160,6 +234,14 @@ pub fn get_counter_value(name: &str) -> u64 {
         }
     });
 
+    let is_average = __AcMode::AVG == *mode;
+    drop(counters);
 
-    result
+    let mut counter = if is_average {
+        get_counter_value(&(name.to_string() + COUNTER_SUFFIX)).0
+    } else {
+        0
+    };
+
+    (result, counter)
 }
--- src/logging.rs
+++ src/logging.rs
@@ -1,14 +1,13 @@
+use crate::counter::{get_counter_value, COUNTER_SUFFIX, __COUNTERS_LIST};
+use bytesize::ByteSize;
+use json::{object, JsonValue};
 use std::fs::File;
-use std::path::{Path};
+use std::io::Write;
+use std::path::Path;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use json::{JsonValue, object};
-use crate::counter::{__COUNTERS_LIST, get_counter_value};
-use std::io::Write;
-use bytesize::ByteSize;
 
 pub fn enable_counters_logging(file: impl AsRef<Path>, interval: Duration) {
-
     let mut file = File::create(file).unwrap();
     let time = Instant::now();
 
@@ -27,11 +26,26 @@ pub fn enable_counters_logging(file: impl AsRef<Path>, interval: Duration) {
             json_values["_time"] = time.elapsed().as_secs_f64().into();
 
             for name in &keys {
-                let value = get_counter_value(&name);
+                // Skip average counters
+                if name.ends_with(COUNTER_SUFFIX) {
+                    continue;
+                }
 
-                let values: [JsonValue; 2] = [value.into(), format!("{}", ByteSize(value)).into()];
+                let (value, avg_counter) = get_counter_value(&name);
 
-                json_values[name] = values.as_ref().into();
+                // Average, use floating point
+                if avg_counter > 0 {
+                    let avg_value = (value as f64) / (avg_counter as f64);
+                    let values: [JsonValue; 2] = [
+                        avg_value.into(),
+                        format!("{}", ByteSize(avg_value as u64)).into(),
+                    ];
+                    json_values[name] = values.as_ref().into();
+                } else {
+                    let values: [JsonValue; 2] =
+                        [value.into(), format!("{}", ByteSize(value)).into()];
+                    json_values[name] = values.as_ref().into();
+                }
             }
 
             writeln!(file, "{}", json_values);
